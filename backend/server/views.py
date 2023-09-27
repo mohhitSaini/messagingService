791078d1a637bca7fa1b54e945472e13ff3d9897
Mohit <mohitsaini3012003@gmail.com>
2023-09-27 16:22:01 +0530

    frontend + backend

---
+++ backend/server/views.py
@@ -0,0 +1,46 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+import requests
+
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+@api_view(['POST'])
+def signup(request):
+    username = request.data['username']
+    secret = request.data['secret']
+    email = request.data['email']
+    first_name = request.data['first_name']
+    last_name = request.data['last_name']
+
+    response = requests.post('https://api.chatengine.io/users/', 
+        data={
+            "username": username,
+            "secret": secret,
+            "email": email,
+            "first_name": first_name,
+            "last_name": last_name,
+        },
+        headers={ "Private-Key": env('CHAT_ENGINE_PRIVATE_KEY') }
+    )
+
+    return Response(response.json(), status=response.status_code)
+    
+@api_view(['POST'])
+def login(request):
+    username = request.data['username']
+    secret = request.data['secret']
+
+
+    response = requests.get('https://api.chatengine.io/users/me/', 
+        headers={ 
+            "Project-ID": env('CHAT_ENGINE_PROJECT_ID'),
+            "User-Name": username,
+            "User-Secret": secret
+        }
+    )
+
+    return Response(response.json(), status=response.status_code)
